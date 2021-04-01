e1eec33eda7b66228081633ded2e9339249ef203
CCBaxter84 <troyceclark@gmail.com>
2021-04-01 14:49:55 -0600

    Got post request working

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,36 @@
+// Import libraries and dependencies
 import express, { Application } from "express";
+import expressLayouts from "express-ejs-layouts";
+import mongoose from "mongoose";
+import indexRouter from "./routes/index";
 import txRouter from "./routes/txRoutes";
+import dotenv from "dotenv";
 
+// Initialize the server and environment variables
 const app: Application = express();
+dotenv.config({ path: __dirname + "/../.env" });
+const PORT = process.env.PORT || 3000;
+const db: string = process.env.mongoURI || "";
+
+// Set the server's view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/../views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+// Connect to the database
+mongoose.connect(db, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("Connected to database"))
+  .catch(error => console.log(error));
+
+// Set the server's routes
+app.use("/", indexRouter);
 app.use("/utxos", txRouter);
 
-app.listen(5000, () => console.log("Server is listening on port 5000"));
+// Set the server to listen
+app.listen(PORT, () => console.log("Server is listening on port 5000"));
--- src/routes/txRoutes.ts
+++ src/routes/txRoutes.ts
@@ -5,20 +5,23 @@ const txRouter = Router();
 // @route GET /utxos/:address
 // @desc  Retreive all transactions for a given address
 txRouter.get("/:address", (req: Request, res: Response) => {
-  res.send("Your mom");
+  res.render("utxos/index");
 });
 
 // @route GET /utxos/:address/:spent
 // @desc  Retrive all spent or unspent transactions for a given address
 txRouter.get("/:address/:spent", (req: Request, res: Response) => {
-
+  res.render("utxos/spent");
 });
 
-
 // @route POST /utxos
-// @desc  Add a new transaction
-txRouter.post("/", (req: Request, res: Response) => {
-
+// @desc  Create a new transaction
+txRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    res.status(201).send("Create");
+  } catch(error) {
+    res.status(500).send(error);
+  }
 });
 
 export default txRouter;
