13a9454399ac23cb28951596be21e74ec0e3131a
CCBaxter84 <troyceclark@gmail.com>
2021-03-31 20:33:48 -0600

    Completed initial setup and db model

---
+++ src/models/transaction.ts
@@ -0,0 +1,36 @@
+import { Model, model, Schema, Document } from "mongoose";
+
+interface ITx extends Document {
+  id: string,
+  txid: string,
+  address: string,
+  amount: number,
+  spent: boolean
+}
+
+const txSchema = new Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  txid: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  spent: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const Transaction: Model<ITx> = model("Transaction", txSchema);
+
+export default Transaction;
